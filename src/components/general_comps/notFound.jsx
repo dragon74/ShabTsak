bc3e11d2804a737c171f4f30f9b31056b571980f
Chaya Segal <chayale90@gmail.com>
2023-10-20 01:36:47 +0300

    text center and components of mui

--- src/components/general_comps/notFound.jsx
+++ src/components/general_comps/notFound.jsx
@@ -1,4 +1,4 @@
-import { Container, CssBaseline } from '@mui/material'
+import { Box, Container, CssBaseline } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 export default function NotFound() {
@@ -6,8 +6,10 @@ export default function NotFound() {
         <div className='not-found-comp'>
             <CssBaseline />
             <Container fixed>
-                <h1>עמוד לא נמצא 404!</h1>
-                <Link to="/">חזרה לעמוד הבית</Link>
+                <Box textAlign={"center"}>
+                    <h1>עמוד לא נמצא 404!</h1>
+                    <Link to="/">חזרה לעמוד הבית</Link>
+                </Box>
             </Container>
         </div>
 
