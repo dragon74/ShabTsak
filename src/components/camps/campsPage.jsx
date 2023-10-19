c5ede0516dd76bdb17d35f6938d3518ce338d67f
Chaya Segal <chayale90@gmail.com>
2023-10-20 01:36:21 +0300

    order rows

--- src/components/camps/campsPage.jsx
+++ src/components/camps/campsPage.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
-import { Box, Button, Container, CssBaseline } from "@mui/material"
+import { Box, Button, Container } from "@mui/material"
 import DialogComp from "../general_comps/dialogComp";
 import CampsList from "./campsList";
 
 const CampsPage = () => {
-
   const [openDialog, setOpenDialog] = useState(false);
-
 
   return (
     <div className="camps-page">
-      <CssBaseline />
       <Container fixed>
-
         <Box style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginTop: '20px' }}>
           <Button variant="contained"
             onClick={() => {
@@ -22,12 +18,8 @@ const CampsPage = () => {
             הוסף בסיס
           </Button>
         </Box>
-
         <CampsList />
-
         <DialogComp openDialog={openDialog} setOpenDialog={setOpenDialog} subject={"camp"} action="Add" />
-
-
       </Container>
     </div>
   )
