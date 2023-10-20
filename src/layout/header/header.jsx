95d89fb076be6b77da26f29960cc63ee3d4f9f7d
niv netanel <nivnetanel@gmail.com>
2023-10-20 13:32:49 +0300

    guards support

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,209 +1,200 @@
-import { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import Grid from '@mui/material/Grid';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { useNavigate } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { theme } from "../../services/theme";
-import Logo from '../../components/general_comps/logo';
-import srcImg from '/images/man.png';
+import Logo from "../../components/general_comps/logo";
+import srcImg from "/images/man.png";
 import { changeDarkMode } from "../../features/featuresSlice";
-import { useDispatch, useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Header = () => {
-    const nav = useNavigate();
-    const dispatch = useDispatch();
+  const nav = useNavigate();
+  const dispatch = useDispatch();
 
-    // Navbar states
-    const [anchorElNav, setAnchorElNav] = useState(null);
-    const [anchorElUser, setAnchorElUser] = useState(null);
-    const [displayBurger, setDisplayBurger] = useState("block");
-    const [displayButtonX, setDisplayButtonX] = useState("none");
+  // Navbar states
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
-    // Navbar functions
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-        setDisplayBurger("none");
-        setDisplayButtonX("block");
-    };
+  // Navbar functions
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-        setDisplayBurger("block");
-        setDisplayButtonX("none");
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    // Open user menu
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  // Open user menu
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    // Close user menu
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  // Close user menu
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    // Dark Mode
-    const { darkMode } = useSelector((myStore) => myStore.featuresSlice);
+  // Dark Mode
+  const { darkMode } = useSelector((myStore) => myStore.featuresSlice);
 
-    const ClickLogout = () => {
-        nav("/");
-        toast.success("With God's help we will fight and win!");
-    };
+  const ClickLogout = () => {
+    nav("/");
+    toast.success("With God's help we will fight and win!");
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-            <AppBar position="static" color={darkMode === false ? 'primary' : 'darkMode'}>
-                <Container maxWidth="lg">
-                    <Grid container justifyContent="space-between" alignItems="center">
-                        {/* big screen */}
-                        <Grid item sx={{ display: { xs: 'none', md: 'block' } }}>
-                            <Logo />
-                        </Grid>
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" color={darkMode === false ? "primary" : "darkMode"}>
+        <Container maxWidth="lg">
+          <Grid container alignItems="center">
+            <Grid item xs={3} md={2}>
+              <Logo />
+            </Grid>
 
-                        {/* small screen */}
-                        <Grid item sx={{ display: { sx: 'flex', md: 'none' } }}>
-                            <IconButton
-                                size="small"
-                                aria-label="account of the current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="white"
-                            >
-                                <MenuIcon sx={{ display: displayBurger }} />
-                                <CloseIcon sx={{ display: displayButtonX }} />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{
-                                    display: { xs: 'block', md: 'none' },
-                                }}
-                            >
-                                <MenuItem
-                                    onClick={() => {
-                                        handleCloseNavMenu();
-                                        nav("/");
-                                    }}
-                                >
-                                    Camps
-                                </MenuItem>
-                                <MenuItem
-                                    onClick={() => {
-                                        handleCloseNavMenu();
-                                        nav("/Schedule");
-                                    }}
-                                >
-                                    schedule
-                                </MenuItem>
-                            </Menu>
-                        </Grid>
+            <Grid item xs={6} md={4} sx={{ textAlign: "center" }}>
+              <Button
+                onClick={() => {
+                  nav("/");
+                }}
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  px: 2,
+                }}
+              >
+                Camps
+              </Button>
+              <Button
+                onClick={() => {
+                  nav("/schedule");
+                }}
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  ml: 2,
+                  px: 2,
+                }}
+              >
+                Schedule
+              </Button>
+              <Button
+                onClick={() => {
+                  nav("/guards");
+                }}
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  ml: 2,
+                  px: 2,
+                }}
+              >
+                Guards
+              </Button>
+            </Grid>
 
-                        <Grid item sx={{ display: { sx: 'block', md: 'none' } }}>
-                            <Logo />
-                        </Grid>
+            <Grid item xs={3} md={6} sx={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button
+                onClick={() => {
+                  dispatch(changeDarkMode());
+                }}
+                color={darkMode === true ? "primary" : "inherit"}
+              >
+                {darkMode === true ? <Brightness7Icon /> : <Brightness4Icon />}
+                {darkMode === false ? "Light" : "Dark"}
+              </Button>
 
-                        <Grid item sx={{ display: { xs: 'none', md: 'flex' } }}>
-                            <Button
-                                onClick={() => {
-                                    nav("/");
-                                }}
-                                sx={{
-                                    color: 'white',
-                                    px: 3,
-                                    py: 3,
-                                }}
-                            >
-                                Camps
-                            </Button>
-                            <Button
-                                onClick={() => {
-                                    nav("/schedule");
-                                }}
-                                sx={{
-                                    color: "white",
-                                    px: 3,
-                                    py: 3,
-                                }}
-                            >
-                                schedule
-                            </Button>
-                        </Grid>
+              <Tooltip title="Avatar">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Avatar" src={srcImg} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: 3 }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={ClickLogout}>Good Day!</MenuItem>
+              </Menu>
+            </Grid>
 
-
-                        <Grid item
-                            sx={{
-                                display: { xs: 'none', md: 'flex', textAlign: "center", px: 3, alignItems: 'center', color: darkMode === true ? '#8ECDDD' : 'yellow' },
-                            }}>
-                            {darkMode === false ? "Light" : 'Dark'}
-                            <IconButton onClick={() => { dispatch(changeDarkMode()) }} color={darkMode === true ? "primary" : "inherit"}>
-                                {darkMode === true ? <Brightness7Icon /> : <Brightness4Icon />}
-                            </IconButton>
-                        </Grid>
-
-                        <Grid item>
-                            <Tooltip title="Avatar">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Avatar" src={srcImg} />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: 3 }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                <MenuItem
-                                    sx={{
-                                        display: { xs: 'block', md: 'none', textAlign: "center", px: 3, alignItems: 'center' },
-                                    }}
-                                >
-                                    {darkMode === false ? "Light" : 'Dark'}
-                                    <IconButton onClick={() => { dispatch(changeDarkMode()) }} color="inherit">
-                                        {darkMode === true ? <Brightness7Icon /> : <Brightness4Icon />}
-                                    </IconButton>
-                                </MenuItem>
-                                <MenuItem onClick={ClickLogout}>Good Day!</MenuItem>
-                            </Menu>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </AppBar>
-        </ThemeProvider >
-    );
+            <Grid item xs={3} sx={{ display: { xs: "block", md: "none" }, textAlign: "center" }}>
+              <Button onClick={handleOpenNavMenu} variant="outlined" color="inherit">
+                <MenuIcon />
+              </Button>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    nav("/");
+                  }}
+                >
+                  Camps
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    nav("/Schedule");
+                  }}
+                >
+                  Schedule
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    nav("/guards");
+                  }}
+                >
+                  Guards
+                </MenuItem>
+              </Menu>
+            </Grid>
+          </Grid>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
