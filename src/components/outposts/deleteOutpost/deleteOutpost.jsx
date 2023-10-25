06ab39299c36906ee01439342c87e56e94ca2935
Chaya Segal <chayale90@gmail.com>
2023-10-25 12:16:10 +0300

    fix import deleteDialog

--- src/components/outposts/deleteOutpost/deleteOutpost.jsx
+++ src/components/outposts/deleteOutpost/deleteOutpost.jsx
@@ -1,10 +1,10 @@
-/* eslint-disable react/prop-types */
 import { IconButton } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
-import DialogDelete from "../../general_comps/dialogs/DialogDelete";
+import DialogDelete from "../../general_comps/dialogs/dialogDelete";
 import { useState } from "react";
 
 //sure dialog
+// eslint-disable-next-line react/prop-types
 const DeleteCamp = ({ item, getOutpostsByCampId }) => {
 
     const [openSureDialog, setOpenSureDialog] = useState(false);
