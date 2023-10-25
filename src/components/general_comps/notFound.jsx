4bf04f29ea9af2d6e015b96e96ffcfd48239342a
Chaya Segal <chayale90@gmail.com>
2023-10-25 16:51:22 +0300

    fix link in not found component

--- src/components/general_comps/notFound.jsx
+++ src/components/general_comps/notFound.jsx
@@ -1,17 +1,21 @@
-import { Box, Container, CssBaseline } from '@mui/material'
+import { Box, Container, CssBaseline, Typography, Link as MuiLink  } from '@mui/material'
 import { Link } from 'react-router-dom'
+import ROUTES from "../../constants/routeConstants"
 
 export default function NotFound() {
     return (
-        <div className='not-found-comp'>
+        <div>
             <CssBaseline />
             <Container fixed>
-                <Box textAlign={"center"}>
-                    <h2>עמוד לא נמצא 404!</h2>
-                    <Link to="/">חזרה לעמוד הבית</Link>
+                <Box textAlign={"center"} mt={4}>
+                    <Typography variant="h4" component="h2" >
+                        עמוד לא נמצא 404!
+                    </Typography>
+                    <MuiLink component={Link} to={ROUTES.HOME}>
+                        חזרה לעמוד הבית
+                    </MuiLink>
                 </Box>
             </Container>
         </div>
-
     )
 }
