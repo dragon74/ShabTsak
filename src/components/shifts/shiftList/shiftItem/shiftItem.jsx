aac9601982e2c5ec49667037326f4ef751e5657d
Chaya Segal <chayale90@gmail.com>
2023-11-10 14:57:32 +0200

    prettier code in pritter extention

--- src/components/shifts/shiftList/shiftItem/shiftItem.jsx
+++ src/components/shifts/shiftList/shiftItem/shiftItem.jsx
@@ -1,44 +1,42 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { TableCell, TableRow } from "@mui/material";
-import ShiftItemActions from './shiftItemActions/shiftItemActions';
+import ShiftItemActions from "./shiftItemActions/shiftItemActions";
 import { getDayOfWeekHebrew } from "@/lib/utils/dateUtils";
-import { useMemo } from 'react';
+import { useMemo } from "react";
 
 ShiftItem.propTypes = {
-    item: PropTypes.object
-}
+  item: PropTypes.object,
+};
 
 export default function ShiftItem({ item }) {
+  const dayNumber = item.dayId;
 
-    const dayNumber = item.dayId;
+  const dayHebrew = useMemo(() => {
+    if (dayNumber) {
+      return getDayOfWeekHebrew(dayNumber);
+    }
+    return "";
+  }, [dayNumber]);
 
-    const dayHebrew = useMemo(() => {
-        if (dayNumber) {
-            return getDayOfWeekHebrew(dayNumber);
-        }
-        return '';
-    }, [dayNumber]);
+  return (
+    <TableRow>
+      <TableCell align="center">{item.dayId}</TableCell>
+      <TableCell align="center">{dayHebrew}</TableCell>
+      <TableCell align="center" direction="asc">
+        {item.fromHour}:00
+      </TableCell>
+      <TableCell align="center">{item.toHour}:00</TableCell>
 
-    return (
-        <TableRow>
-            <TableCell align="center">{item.dayId}</TableCell>
-            <TableCell align="center">{dayHebrew}</TableCell>
-            <TableCell align="center" direction="asc" >{item.fromHour}:00</TableCell>
-            <TableCell align="center">{item.toHour}:00</TableCell>
-
-            <TableCell
-                align="center"
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-
-                <ShiftItemActions item={item} />
-
-            </TableCell>
-        </TableRow>
-    )
+      <TableCell
+        align="center"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ShiftItemActions item={item} />
+      </TableCell>
+    </TableRow>
+  );
 }
-
