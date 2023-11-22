903792809818ebe99342bcc9236dc3339c00461d
Hadas <hadas.amitzut@sap.com>
2023-11-22 10:59:29 +0200

    firs day and last day

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -68,3 +68,15 @@ export const getDateAndTime = (date, time) => {
     theDate.setHours(theTime);
     return theDate;
 }
+
+export const getFirstDayAndLastDayOfCurrentWeek = () =>{
+    let curr = new Date; // get current date
+    let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+    let last = first + 6; // last day is the first day + 6
+    let firstday = formatDate(new Date(curr.setDate(first)),'ddMMyyyy');
+    let lastday = formatDate(new Date(curr.setDate(last)),'ddMMyyyy');
+    return {
+        first: firstday,
+        last: lastday
+    }
+}
