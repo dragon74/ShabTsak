9888a63c817306de35a0c86e1d91d35be91eaf63
niv netanel <nivnetanel@gmail.com>
2024-01-18 11:11:44 +0200

    inviting the relevant guard(by his email)  into the event at  google calendar

--- src/components/ShiftSchedule/AddToCalendar.jsx
+++ src/components/ShiftSchedule/AddToCalendar.jsx
@@ -1,37 +1,37 @@
-// Import necessary libraries and components
 import { toast } from "react-toastify";
 import { Button } from "@mobiscroll/react";
 
-// AddToCalendar component
-const AddToCalendar = ({ shibuts, guards, outposts }) => {
+const formatToISOString = (date) => date.toISOString().replace(/[-:.]/g, "");
+
+const AddToCalendar = ({ shibuts = {}, guards, outposts }) => {
+  if (!shibuts.campId) {
+    toast.error("אנא בחר מחנה לפני הוספת האירוע ליומן");
+    return;
+  }
+
+  if (!shibuts || Object.keys(shibuts).length === 0) {
+    toast.error("אין נתוני שיבוצים זמינים להוספה ליומן");
+    return;
+  }
+
   const addToGoogleCalendar = () => {
-    if (!shibuts.campId) {
-      toast.error("אנא בחר מחנה לפני הוספת האירוע ליומן");
-      return;
+    const { guardId, guardName, start, end, outpostId } = shibuts;
+
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    const startTime = formatToISOString(startDate);
+    const endTime = formatToISOString(endDate);
+
+    const guard = guards.find((g) => g.value === guardId);
+    const outpost = outposts.find((o) => o.id === outpostId);
+
+    if (!guard) {
+      throw new Error("שומר לא נמצא עבור השיבוץ");
     }
 
-    if (!shibuts || Object.keys(shibuts).length === 0) {
-      toast.error("אין נתוני שיבוצים זמינים להוספה ליומן");
-      return;
-    }
-
-    const startDate = new Date(shibuts.start);
-    const endDate = new Date(shibuts.end);
-
-    const startTime = startDate.toISOString().replace(/[-:.]/g, "");
-    const endTime = endDate.toISOString().replace(/[-:.]/g, "");
-
-    const guard = guards.find((g) => g.value === shibuts.guardId);
-    const outpost = outposts.find((o) => o.id === shibuts.outpostId);
-
-    if (guard) {
-      const eventLink = `https://www.google.com/calendar/event?action=TEMPLATE&text=משמרת+לשומר+${shibuts.guardName}&dates=${startTime}/${endTime}&details=שומר:+${shibuts.guardName},+עמדה:+${outpost.name},+אימייל:+${guard.guardMail},+טלפון:+${guard.guardPhone}`;
-      window.open(eventLink, "_blank");
-    } else {
-      console.error("שומר לא נמצא עבור שיבוץ:", shibuts);
-      toast.error("שגיאה: שומר לא נמצא עבור השיבוץ");
-      return;
-    }
+    const eventLink = `https://www.google.com/calendar/event?action=TEMPLATE&text=משמרת+לשומר+${guardName}&dates=${startTime}/${endTime}&details=שומר:+${guardName}%0Aעמדה:+${outpost.name}%0Aאימייל:+${guard.guardMail}%0Aטלפון:+${guard.guardPhone}&add=${guard.guardMail}`;
+    window.open(eventLink, "_blank");
 
     toast.success("קישור לאירוע ביומן של גוגל נוצר בהצלחה");
   };
